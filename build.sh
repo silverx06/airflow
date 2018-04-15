a6c62c5bb824115c742ec46dd07d95b61f1726b9
silverx06 <38399569+silverx06@users.noreply.github.com>
2018-04-15 15:38:50 +0000

    [fix] build failed

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+TAG=$1
+
 SPARK_VERSION=2.3.0
 HADOOP_VERSION=2.7.5
 
 docker build \
        --build-arg="SPARK_VERSION=${SPARK_VERSION}" \
-       --build-arg="HADOOP_VERSION=${HADOOP_VERSION}" 
+       --build-arg="HADOOP_VERSION=${HADOOP_VERSION}" \
+       --tag ${TAG} \
        .
